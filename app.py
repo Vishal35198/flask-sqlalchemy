153e9860c0f311a6b4e504608932f4e8782770bc
Vishal Chaurasiya <129888819+Vishal35198@users.noreply.github.com>
2024-12-16 11:20:35 +0530

    using bootstrap

--- app.py
+++ app.py
@@ -3,9 +3,22 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+    'id': 1,
+    'title': 'Machine learning engineer',
+    'location': 'noida'
+    },
+    {
+        'id':2,
+        'title':'Data Sceinec engineer',
+        'location':'Delhi'
+    }
+]
+
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('home.html',jobs = JOBS)
 
 if __name__ =="__main__":
     app.run(debug=True)
